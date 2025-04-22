a60429e2c8751530fdd106e1c38d0797f80151b6
sudharsan <sudharsannadar1@gmail.com>
2025-04-22 23:07:11 +0530

    compleated the backend and ready for deploy

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ import nltk
 from nltk.corpus import cmudict
 from Levenshtein import distance as levenshtein_distance
 from dotenv import load_dotenv
-
+#start
 load_dotenv()
-# Ensure CMU Pronouncing Dictionary is downloaded
 try:
     pron_dict = cmudict.dict()
 except LookupError:
@@ -21,7 +20,7 @@ except LookupError:
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Database configuration (Step 4: Update with your actual Render DATABASE_URL)
+# Database configuration 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -111,5 +110,5 @@ def analyze_speech():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Only needs to run once to create tables
+        db.create_all()  
     app.run(host='0.0.0.0', port=8000, debug=True)
